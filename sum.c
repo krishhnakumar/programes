3c417ba00cbe4d35b41a6d94397e29e05ce640b5
krishhnakumar <kkr00730@gmail.com>
2017-01-20 14:25:22 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public static void main(String arg[])
+{
+int n=100;
+  int sum=0;
+  for(int i=0;i<=100;i++)
+  {
+      sum=sum+1;
+      }
+      System.out.printf("the sum is ",sum);
+      }
+      
