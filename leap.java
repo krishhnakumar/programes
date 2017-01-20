d34cca006e50a7ff9c0e992d2e5b737c11d0b9c0
krishhnakumar <kkr00730@gmail.com>
2017-01-20 14:33:10 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,17 @@
+impoer java.utile.Scanner;
+public Static void main(String arg[])
+{
+     int year;
+     sc=new Scanner(System.in);
+     System.out.println("Enete the year");
+     year=sc.nextInt();
+     if((year%400==0)||(year%4==0) &&(year%100!=0))
+     {
+          System.out.print("leaper");
+          }
+          else
+          System.out.print("not aleaper");
+          }
+          }
+          }
+          
