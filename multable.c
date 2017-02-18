0e70150da2d7b2c352ff8fe25d3cc75bbe57f869
krishhnakumar <kkr00730@gmail.com>
2017-02-18 23:46:14 +0530

    Update multable.c

--- multable.c
+++ multable.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int i,j,n;
+int i,j,k;
 printf("enter i and n value:");
-scanf("%d %d ",&j,&n);
-for(i=j;i<=n;i++)
+scanf("%d %d ",&j,&k);
+for(i=j;i<=k;i++)
 {
- printf("%d*%d=%d\n",i,n,i*n);
+ printf("%d*%d=%d\n",i,k,i*k);
 }
 return 0;
 }
