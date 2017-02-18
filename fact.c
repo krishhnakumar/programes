c9742b7a5d3b0ad589921dd8494dd1320c1d969d
krishhnakumar <kkr00730@gmail.com>
2017-02-18 23:41:40 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
- int i,num,fact=1;
+ int i,num,fac=1;
  printf("enter num:");
  scanf("%d",&num);
  for(i=1;i<=num;i++)
  {
-  fact=fact*i;
+  fac=fac*i;
  }
  printf("%d",fact);
  return 0;
