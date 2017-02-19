1d38f15d7d1b56e057bd51c5fd68efca3efee837
krishhnakumar <kkr00730@gmail.com>
2017-02-19 17:58:09 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int lo=0;
+	int hig=0;
+	int c=0;
+	printf("\nEnter lower limit");
+	scanf("%d",&lo);
+	printf("\nEnter upper limit");
+	scanf("%d",&hig);
+	for(int i=lo;i<=hig;i++)
+	{
+		if((i%2)!=0)
+		printf("\t%d",i);
+	}
+return 0;
+}
