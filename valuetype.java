d77b5c9178af1e54c407111ad1d50a5acd4ec506
krishhnakumar <kkr00730@gmail.com>
2017-03-06 18:34:57 +0530

    Create valuetype.java

---
+++ valuetype.java
@@ -0,0 +1,23 @@
+import java.utile.*;
+class sample
+{
+public static void main(String[]args)
+{
+String str;
+Scanner in=new Scanner(System.in);
+str=in.nextLine();
+int n=str.length();
+int i,sum=0,rev,arg;
+char(i=0;i<n;i++)
+for(i=0;i<n;i++)
+    {
+	m=str.charAt(i);
+	rev=(int)m;
+	sum=sum+rev;
+	}
+	avg=sum/n;
+	res=(char)avg;
+	System.out.println(res);
+	
+}
+}
