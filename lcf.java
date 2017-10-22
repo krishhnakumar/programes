f329b325f6dfaaed98312120cfb8406c86cf6b9f
krishhnakumar <kkr00730@gmail.com>
2017-10-22 18:00:06 +0530

    Create lcf.java

---
+++ lcf.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.lang.*;
+    public class count{
+        public static void main(String args[]) throws IOException{
+          System.out.println("Enter the char:");
+          int i = i.nextint();
+          int j = j.nextint();
+             if(i<j)
+             {
+          System.out.println("ans is "+i);
+            }
+            else
+            {
+            System.out.println("ans is "+j);
+            }
+        }
+      }
