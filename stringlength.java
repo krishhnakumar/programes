b353a06481418e784beb37691abf33759f18332f
krishhnakumar <kkr00730@gmail.com>
2017-10-22 17:57:03 +0530

    Create stringlength.java

---
+++ stringlength.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+    public class count{
+        public static void main(String args[]) throws IOException{
+          BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+          System.out.println("Enter the char:");
+          String str = buff.readLine();
+             int l=str.length();
+          System.out.println("the length of string is "+l);
+            }
+        }
+      }
